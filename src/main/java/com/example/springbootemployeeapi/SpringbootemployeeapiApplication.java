67f1fbbca8ce2e4daa05584b4ae9e442ee45d09c
mayur singhal <mayursinghal383@gmail.com>
2019-08-05 14:34:41 +0530

    with eureka server

--- src/main/java/com/example/springbootemployeeapi/SpringbootemployeeapiApplication.java
+++ src/main/java/com/example/springbootemployeeapi/SpringbootemployeeapiApplication.java
@@ -2,12 +2,18 @@ package com.example.springbootemployeeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringbootemployeeapiApplication {
-
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootemployeeapiApplication.class, args);
+		//System.out.println("hello from);
 	}
+	
 
 }
--- src/main/java/com/example/springbootemployeeapi/resources/OperationsOnEmployee.java
+++ src/main/java/com/example/springbootemployeeapi/resources/OperationsOnEmployee.java
@@ -3,7 +3,6 @@ package com.example.springbootemployeeapi.resources;
 import java.util.List;
 
 import javax.ws.rs.DELETE;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
